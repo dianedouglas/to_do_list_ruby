e0d25407d36edf05db80237615a7e24f119d06dd
Amy Vaillancourt-Sals & Diane Douglas <student@epicodus.com>
2014-07-30 09:52:50 -0700

    add ui for creating tasks and listing them

--- todo.rb
+++ todo.rb
@@ -1,16 +1,18 @@
 require './lib/task'
 
+@list = []
+
 def main_menu
   loop do
     puts "Welcome to the main menu. Please select from the following"
     puts "'a' to add a task. 'l' to list all tasks. 'x' to exit."
 
     main_choice = gets.chomp
-    if main_choice = 'a'
-     add_task
-    elsif main_choice = 'l'
+    if main_choice == 'a'
+     create_new_task
+    elsif main_choice == 'l'
      list_tasks
-    elsif main_choice = 'x'
+    elsif main_choice == 'x'
      puts "Good-bye..."
      exit
     else
@@ -19,9 +21,18 @@ def main_menu
   end
 end
 
-def add_task
+def create_new_task
+  puts "Please enter a description of your task."
+  user_description = gets.chomp
+  new_task = Task.new(user_description)
+  @list.push(new_task)
 end
 
 def list_tasks
+  puts "Here are your tasks:"
+  @list.each do |task|
+    puts task.description
+  end
 end
 
+main_menu
